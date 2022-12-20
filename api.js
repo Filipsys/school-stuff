c55a1275935d44d8438075a14d6ed88aee7f7cf8
Filipsys <93247526+Filipsys@users.noreply.github.com>
2022-12-20 12:28:11 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,13 @@
 fetch('http://example.com/data')
   .then(response => response.json())
   .then(data => console.log(data));
+
+// OR
+
+fetch('http://example.com/data', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({key: 'value'})
+});
