60ac30772b2c897e00e7ebf6314918f16cd53178
Filipsys <93247526+Filipsys@users.noreply.github.com>
2022-12-21 18:13:46 +0100

    Create pi.js

---
+++ pi.js
@@ -0,0 +1,9 @@
+// W JavaScript, wartość π (pi) może być uzyskana za pomocą właściwości Math.PI. Oto przykład tego, jak można jej użyć:
+
+console.log(Math.PI);  // Output: 3.141592653589793
+// Można również używać wartości π w wyrażeniach i operacjach matematycznych tak samo, jak każdej innej wartości liczbowej. Na przykład:
+
+let promień = 5;
+let obwód = 2 * Math.PI * promień
+;
+console.log(obwód);  // Output: 31.41592653589793
