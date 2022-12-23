b06869218344c1a911bf2e27ac7bb329c331a8e6
Filipsys <93247526+Filipsys@users.noreply.github.com>
2022-12-23 22:00:18 +0100

    Update petle.js

--- PETLE/petle.js
+++ PETLE/petle.js
@@ -6,20 +6,18 @@ let isEven;
 
 // Rozpoczęcie pętli
 for (i; i <= 10; i++) {
-	if (i % 2 == 1) {								/* Sprawdzanie czy numer jest parzysty */
+	if (i % 2 == 1) {						/* Sprawdzanie czy numer jest parzysty */
 		isEven = "parzyste";
 	} else {
 		isEven = "nieparzyste";
 	}
 
-	document.write(`${i} jest ${isEven}<br>`);		/* Drukowanie wyniku */
+	document.write(`${i} jest ${isEven}<br>`);						/* Drukowanie wyniku */
 }
 
 
-// --------------------------------------------------
 document.write(`-------------------------------------`);
 // --------------------------------------------------
-
 
 // SKRYPT SPRAWDZAJĄCY CZY SUMA LICZB BĘDZIE PATRZYSTA
 
@@ -34,7 +32,7 @@ let numberSum = 0;
 
 // Rozpoczęcie pętli while
 while (numberA <= numberB) {
-	if (numberA % 2 == 0) {				/* Sprawdzanie czy numer jest parzysty, jeśli tak to dodajemy */
+	if (numberA % 2 == 0) {						/* Sprawdzanie czy numer jest parzysty, jeśli tak to dodajemy */
 		numberSum += numberA;
 	}
 	numberA++;
@@ -46,9 +44,6 @@ console.log(`Suma liczb parzystych od ${numberAConst} do ${numberBConst} wynosi:
 
 // --------------------------------------------------
 
-// --------------------------------------------------
-
-
 // Tworzenie zmiennych
 let m = 4;
 let n = 40;
@@ -57,18 +52,15 @@ let numberString = "";
 
 // Rozpoczęcie pętli
 for (m; m < n; m++) {
-	if (m % 2 != 0 || m % 7 == 0) {				/* Sprawdzanie czy pętla zawiera liczbę nieparzystą lub podzielną przez 7 */
+	if (m % 2 != 0 || m % 7 == 0) {						/* Sprawdzanie czy pętla zawiera liczbę nieparzystą lub podzielną przez 7 */
 		numberString += `${m} `;
 	}
 }
 
-alert(numberString);							/* Drukowanie wyniku za pomocą alertu */
+alert(numberString);						/* Drukowanie wyniku za pomocą alertu */
 
 
 // --------------------------------------------------
-
-// --------------------------------------------------
-
 
 // Tworzenie zmiennych
 let passwdInput = prompt(`Podaj hasło poniżej.`);
@@ -77,10 +69,10 @@ let running = true
 
 // Rozpoczęcie pętli while
 while (running) {
-	if (passwdInput == passwd) {			/* Sprawdza czy hasło wpisane jest równe haśle zapisanemu w zmiennej, jeśli jest równe, wyjdź z pętli (niebezpieczne ponieważ oosba mogłaby to łatwo przeczytać) */
+	if (passwdInput == passwd) {						/* Sprawdza czy hasło wpisane jest równe haśle zapisanemu w zmiennej, jeśli jest równe, wyjdź z pętli (niebezpieczne ponieważ oosba mogłaby to łatwo przeczytać) */
 		running = false
 	} else {
-		passwdInput = prompt(`Podaj hasło poniżej.`);	/* Jeśli hasło nie jest poprawne, ponownie zapytaj */
+		passwdInput = prompt(`Podaj hasło poniżej.`);						/* Jeśli hasło nie jest poprawne, ponownie zapytaj */
 	}
 }
 // Drukowanie komunikatu
@@ -89,27 +81,16 @@ console.log(`Well done! You have entered the correct password!`);
 
 // --------------------------------------------------
 
-// --------------------------------------------------
-
-
 // Tworzenie zmiennych
 let a = 1;
 let b = 100;
 
 // Rozpoczęcie pętli while
 while (a <= 100) {
-	if (a % 3 == 0 || a % 5 == 0) {			/* Sprawdzanie czy liczba jest podzielna przez 3 lub 5 ORAZ niepodzielna przez 15, drukujemy wynik */
+	if (a % 3 == 0 || a % 5 == 0) {						/* Sprawdzanie czy liczba jest podzielna przez 3 lub 5 ORAZ niepodzielna przez 15, drukujemy wynik */
 		if (a % 15 != 0) {
 			console.log(`${a}`);
 		}
 	}
-
 	a++;
 }
-
-
-//* ZADANIE 1 ZROBIONE
-//* ZADANIE 2 ZROBIONE
-//* ZADANIE 3 ZROBIONE
-//* ZADANIE 4 ZROBIONE
-//* ZADANIE 5 ZROBIONE
