2ec4071a15b1aa1910672bb5e46b477ed8cf0e84
Filipsys <93247526+Filipsys@users.noreply.github.com>
2023-01-05 23:19:20 +0100

    Update api.js

--- Filip/api.js
+++ Filip/api.js
@@ -1,13 +1,30 @@
-fetch('http://example.com/data')
-  .then(response => response.json())
-  .then(data => console.log(data));
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
 
-// OR
+function randText() {
+  const textList = ["Hello!", "Hi", "What's up?", "Hello world!", "Hey"];
 
-fetch('http://example.com/data', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({key: 'value'})
+  return textList[(Math.floor(Math.random() * textList.length))];
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/api/hello", (req, res) => {
+  res.send({
+    message: randText()
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const server = app.listen(3000, () => {
+  const address = server.address();
+  if (address) {
+    console.log(`Server running at http://${address.address}:${address.port}/`);
+    console.log(`API listening on port 3000`);
+  }
 });
